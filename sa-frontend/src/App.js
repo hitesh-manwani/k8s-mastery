711725dbb25093281c44c90b78dfa9d562763511
root <root@ip-10-0-2-154.us-west-2.compute.internal>
2022-08-30 08:16:04 +0000

    k8s-mastery

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://34.219.70.70:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
